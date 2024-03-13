37536a0fe2eaa6ee2badce3b170401d84d73f4bc
purna1031 <157563005+purna1031@users.noreply.github.com>
2024-03-13 21:47:18 +0530

    first commit

--- x.js
+++ x.js
@@ -1,1 +1,19 @@
-console.log("heyy");
+const express = require('express')
+const app = express();
+const port=3000;
+
+app.set("view engine","ejs");
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/signin', (req, res) => {
+    res.send('Im in home')
+  })
+  app.get('/signup', (req, res) => {
+    res.render('signup');
+  })
+app.listen(3000,()=>{
+    console.log(`app listening on port ${port}`);
+})
